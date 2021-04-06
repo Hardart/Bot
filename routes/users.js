8d72fcac92ee11bc62bd9a680ffbbde3ae6024f2
Hardart <kaino@bk.ru>
2021-04-07 00:37:24 +0300

    changes in routes

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const { Padavan, RegData } = require('../models/padavans')
-<<<<<<< HEAD
-router.use(formidableMiddleware())
-=======
 router.use(express.json())
 router.get('/', (req, res) => {
 	res.send("")
 })
->>>>>>> 10dd844357b640fa20445e671111365a3f558754
 
 router.post('/', async (req, res) => {
 	console.log(req.body)
