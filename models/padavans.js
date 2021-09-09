7bb7c384916a92530345db69b3230fdbe64069ce
Hardart <kaino@bk.ru>
2021-09-09 20:05:54 +0300

    some changes

--- models/padavans.js
+++ models/padavans.js
@@ -1,25 +1,28 @@
 const mongoose = require('mongoose')
 
 const Pdvn = mongoose.Schema({
-	vk_id: String,
-	full_name: String,
-	ren_login: String,
-	ren_pass: String,
-	w_code: String,
-	coach_id: Number,
-	options: {
-		hasQ: Boolean,
-		changeCoach: Boolean
-	}
+  vk_id: Number,
+  full_name: String,
+  ren_login: String,
+  ren_pass: String,
+  w_code: String,
+  coach_id: Number,
 })
 
 const Reg_data = mongoose.Schema({
-	ren_login: String,
-	ren_pass: String,
-	w_code: String
+  ren_login: String,
+  ren_pass: String,
+  w_code: String,
+})
+
+const Test = mongoose.Schema({
+  title: String,
+  prefix: String,
+  number: Number,
 })
 
 module.exports = {
-	Padavan: mongoose.model('Padavan', Pdvn),
-	RegData: mongoose.model('Reg_data', Reg_data)
+  Padavan: mongoose.model('Padavan', Pdvn),
+  RegData: mongoose.model('Reg_data', Reg_data),
+  Test: mongoose.model('Test', Test),
 }
--- models/reg_data.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const Reg_data = mongoose.Schema({
-	ren_login: String,
-	ren_pass: String,
-	w_code: String
-})
-
-module.exports = mongoose.model('Reg_data', Reg_data)
