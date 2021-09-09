88897650781c4929a2f5116eed0fe37b2715bd8a
Hardart <kaino@bk.ru>
2021-09-09 19:53:12 +0300

    change path for images

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ require('dotenv/config')
 const fs = require('fs')
 const path = require('path')
 const kbd = require('./keyboards')
-const { newKeybord } = require('./functions')
+const { newKeybord, photo } = require('./functions')
 const db = require('./dbConnect')
 const padavans = require('./query')
 const scenes = require('./scenes')
@@ -22,11 +22,11 @@ const TOKEN = process.env.VK_TOKEN
 const app = express()
 const PORT = process.env.PORT || 80
 const bot = new VkBot({
-	token: TOKEN,
-	confirmation: process.env.VK_CONFIRM,
+  token: TOKEN,
+  confirmation: process.env.VK_CONFIRM,
 })
 
-// photo('Castle.png', process.env.VK_ID, TOKEN, bot) // отправка фото
+// photo('Insta.png', process.env.VK_ID, TOKEN, bot) // отправка фото
 
 app.set('views', './views')
 app.set('view engine', 'pug')
@@ -46,13 +46,13 @@ const cleanPoints = scenes.cleanPoints
 const sendToCoach = scenes.send
 const session = new Session()
 const stage = new Stage(
-	addScene,
-	changeScene,
-	deleteScene,
-	addPadScene,
-	deletePadScene,
-	cleanPoints,
-	sendToCoach
+  addScene,
+  changeScene,
+  deleteScene,
+  addPadScene,
+  deletePadScene,
+  cleanPoints,
+  sendToCoach
 )
 
 bot.use(session.middleware())
@@ -60,106 +60,106 @@ bot.use(stage.middleware())
 // --------------------------------------
 
 bot.command('/config', (ctx) => {
-	ctx.reply(`Отлично!\nВот основные настройки`, null, kbd.mainMenu)
+  ctx.reply(`Отлично!\nВот основные настройки`, null, kbd.mainMenu)
 })
 
 bot.command('Test', async (ctx) => {
-	fs.readFile('./files/coach.json', 'utf8', (err, data) => {
-		if (err) {
-			console.log(err)
-		} else {
-			let res = JSON.parse(data)
-			const newUser = {
-				name: 'Dana',
-				age: 27,
-				gender: 'Female',
-				department: 'Organic',
-				car: 'Mazda',
-			}
-			if (!res.CC_99) {
-				res.CC_99 = newUser
-				fs.writeFileSync(file, JSON.stringify(res))
-			}
-			console.log(res.CC_99)
-		}
-	})
-	// console.log(user)
-	await ctx.reply('ok')
+  fs.readFile('./files/coach.json', 'utf8', (err, data) => {
+    if (err) {
+      console.log(err)
+    } else {
+      let res = JSON.parse(data)
+      const newUser = {
+        name: 'Dana',
+        age: 27,
+        gender: 'Female',
+        department: 'Organic',
+        car: 'Mazda',
+      }
+      if (!res.CC_99) {
+        res.CC_99 = newUser
+        fs.writeFileSync(file, JSON.stringify(res))
+      }
+      console.log(res.CC_99)
+    }
+  })
+  // console.log(user)
+  await ctx.reply('ok')
 })
 
 bot.on(async (ctx) => {
-	const payload = ctx.message.payload
-	const userMsg = ctx.message.text
-	const userID = ctx.message.from_id
-	const user = await api('users.get', {
-		user_ids: userID,
-		access_token: TOKEN,
-	}).then((data) => data.response[0])
-
-	// КНОПКА
-	if (payload) {
-		const btn = JSON.parse(payload)
-		switch (btn.value) {
-			case 'main_menu':
-				ctx.reply(`Вы вернулись в главное меню...`, null, kbd.mainMenu)
-				break
-			case 'delete_coach':
-				ctx.scene.enter('deleteCoach')
-				break
-			case 'change_coach':
-				ctx.scene.enter('changeCoach')
-				break
-			case 'add_coach':
-				ctx.scene.enter('addCoach')
-				break
-			case 'coach_config':
-				ctx.reply('Выбери действие', null, kbd.coachMenu)
-				break
-			case 'stepBack':
-				ctx.scene.enter('changeCoach', [1])
-				break
-			case 'padavan_config':
-				ctx.reply('Выбери действие', null, kbd.padavanMenu)
-				break
-			case 'add_padavan':
-				ctx.scene.enter('addPadavan')
-				break
-			case 'send_to_coach':
-				ctx.scene.enter('sendToCoach')
-				break
-			case 'clear_data':
-				ctx.scene.enter('clearData')
-				break
-			case 'delete_padavan':
-				ctx.scene.enter('deletePadavan')
-				break
-			default:
-				ctx.reply(`Вы нажали кнопку, но у нее нет никакого действия`)
-		}
-		// УЧЕНИК
-	} else {
-		ctx.reply('Без кода доступа ты не сможешь настроить мои программы')
-	}
+  const payload = ctx.message.payload
+  const userMsg = ctx.message.text
+  const userID = ctx.message.from_id
+  const user = await api('users.get', {
+    user_ids: userID,
+    access_token: TOKEN,
+  }).then((data) => data.response[0])
+  // КНОПКА
+  if (payload) {
+    const btn = JSON.parse(payload)
+    switch (btn.value) {
+      case 'main_menu':
+        ctx.reply(`Вы вернулись в главное меню...`, null, kbd.mainMenu)
+        break
+      case 'delete_coach':
+        ctx.scene.enter('deleteCoach')
+        break
+      case 'change_coach':
+        ctx.scene.enter('changeCoach')
+        break
+      case 'add_coach':
+        ctx.scene.enter('addCoach')
+        break
+      case 'coach_config':
+        ctx.reply('Выбери действие', null, kbd.coachMenu)
+        break
+      case 'stepBack':
+        ctx.scene.enter('changeCoach', [1])
+        break
+      case 'padavan_config':
+        ctx.reply('Выбери действие', null, kbd.padavanMenu)
+        break
+      case 'add_padavan':
+        ctx.scene.enter('addPadavan')
+        break
+      case 'send_to_coach':
+        ctx.scene.enter('sendToCoach')
+        break
+      case 'clear_data':
+        ctx.scene.enter('clearData')
+        break
+      case 'delete_padavan':
+        ctx.scene.enter('deletePadavan')
+        break
+      default:
+        ctx.reply(`Вы нажали кнопку, но у нее нет никакого действия`)
+    }
+  }
+  // УЧЕНИК
+  else {
+    ctx.reply('Без кода доступа ты не сможешь настроить мои программы')
+  }
 })
 
 app.post('/', bot.webhookCallback)
 
 async function start() {
-	try {
-		await mongoose.connect(process.env.DB_CONN, {
-			useFindAndModify: false,
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		})
-		db.connection().connect((err) => {
-			err ? console.log(err) : console.log('БД подключена')
-		})
-		app.listen(PORT, () => {
-			console.log('Сервер запустился')
-		})
-	} catch (e) {
-		console.log(e)
-	}
+  try {
+    await mongoose.connect(process.env.DB_CONN, {
+      useFindAndModify: false,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    db.connection().connect((err) => {
+      err ? console.log(err) : console.log('БД подключена')
+    })
+    app.listen(PORT, () => {
+      console.log('Сервер запустился')
+    })
+  } catch (e) {
+    console.log(e)
+  }
 }
 
 start()
--- functions.js
+++ functions.js
@@ -5,67 +5,67 @@ const api = require('node-vk-bot-api/lib/api')
 const Markup = require('node-vk-bot-api/lib/markup')
 
 module.exports = {
-	photo: async function (filename, userID, token, bot, mess = '') {
-		const url = await api('photos.getMessagesUploadServer', {
-			peer_id: userID,
-			access_token: token,
-		}).then((url) => url.response.upload_url)
+  photo: async function (filename, userID, token, bot, mess = '') {
+    const url = await api('photos.getMessagesUploadServer', {
+      peer_id: userID,
+      access_token: token,
+    }).then((url) => url.response.upload_url)
 
-		let img = fs.readFileSync('./img/' + filename)
-		const form = new FormData()
-		form.append('photo', img, {
-			contentType: 'img/png',
-			name: 'photo',
-			filename: filename,
-		})
-		const object = await fetch(url, {
-			method: 'POST',
-			body: form,
-		}).then((obj) => obj.json())
-		await api('photos.saveMessagesPhoto', {
-			photo: object.photo,
-			server: object.server,
-			hash: object.hash,
-			access_token: token,
-		}).then((data) => {
-			const user = data.response[0]
-			const attach = 'photo' + user.owner_id + '_' + user.id
-			bot.sendMessage(userID, mess, attach)
-		})
-	},
-	newKeybord: function (users) {
-		buttons = []
-		let summ = users.length
-		let columns = 2
-		if (summ % 4 == 0 && summ > 5) {
-			columns = 4
-		} else if (summ % 3 == 0 || summ > 10) {
-			columns = 3
-		}
+    let img = fs.readFileSync('./assets/img/' + filename)
+    const form = new FormData()
+    form.append('photo', img, {
+      contentType: 'img/png',
+      name: 'photo',
+      filename: filename,
+    })
+    const object = await fetch(url, {
+      method: 'POST',
+      body: form,
+    }).then((obj) => obj.json())
+    await api('photos.saveMessagesPhoto', {
+      photo: object.photo,
+      server: object.server,
+      hash: object.hash,
+      access_token: token,
+    }).then((data) => {
+      const user = data.response[0]
+      const attach = 'photo' + user.owner_id + '_' + user.id
+      bot.sendMessage(userID, mess, attach)
+    })
+  },
+  newKeybord: function (users) {
+    buttons = []
+    let summ = users.length
+    let columns = 2
+    if (summ % 4 == 0 && summ > 5) {
+      columns = 4
+    } else if (summ % 3 == 0 || summ > 10) {
+      columns = 3
+    }
 
-		for (let i = 0; i < 10; i++) {
-			arrayOfButtons = []
-			for (let j = i * columns; j < (i + 1) * columns; j++) {
-				if (users[j]) {
-					arrayOfButtons.push(users[j])
-				}
-			}
-			if (arrayOfButtons[0]) {
-				buttons.push(arrayOfButtons)
-			}
-		}
-		let cancel = Markup.button('Отменить', 'negative', { value: 'cancel' })
-		buttons.push([cancel])
-		return buttons
-	},
-	sendRequest: function (method, url, body = null) {
-		// const headers = {
-		// 	'Content-Type': 'application/json',
-		// }
-		return fetch(url, {
-			method: method,
-			body: body ? JSON.stringify(body) : null,
-			// headers: headers,
-		}).then((obj) => obj.json())
-	},
+    for (let i = 0; i < 10; i++) {
+      arrayOfButtons = []
+      for (let j = i * columns; j < (i + 1) * columns; j++) {
+        if (users[j]) {
+          arrayOfButtons.push(users[j])
+        }
+      }
+      if (arrayOfButtons[0]) {
+        buttons.push(arrayOfButtons)
+      }
+    }
+    let cancel = Markup.button('Отменить', 'negative', { value: 'cancel' })
+    buttons.push([cancel])
+    return buttons
+  },
+  sendRequest: function (method, url, body = null) {
+    // const headers = {
+    // 	'Content-Type': 'application/json',
+    // }
+    return fetch(url, {
+      method: method,
+      body: body ? JSON.stringify(body) : null,
+      // headers: headers,
+    }).then((obj) => obj.json())
+  },
 }
