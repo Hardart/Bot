10209a2efe8a361ee34ad19832650d95d5bbd17e
hardart <kaino@bk.ru>
2021-09-10 20:20:15 +0300

    add test config

--- mongoModels.js
+++ mongoModels.js
@@ -1,36 +1,35 @@
 const mongoose = require('mongoose')
 
 const Padavan = mongoose.Schema({
-  vk_id: Number,
-  full_name: String,
-  ren_login: String,
-  ren_pass: String,
-  w_code: String,
-  coach_id: Number,
+   vk_id: Number,
+   full_name: String,
+   ren_login: String,
+   ren_pass: String,
+   w_code: String,
+   coach_id: Number,
 })
 
 const Reg_data = mongoose.Schema({
-  ren_login: String,
-  ren_pass: String,
-  w_code: String,
+   ren_login: String,
+   ren_pass: String,
+   w_code: String,
 })
 
 const Test = mongoose.Schema({
-  title: String,
-  prefix: String,
-  number: Number,
-  points: Number,
+   title: String,
+   prefix: String,
+   points: Number,
 })
 
 const Coach = mongoose.Schema({
-  vk_id: Number,
-  full_name: String,
-  coach_id: Number,
+   vk_id: Number,
+   full_name: String,
+   coach_id: Number,
 })
 
 module.exports = {
-  Padavan: mongoose.model('Padavan', Padavan),
-  RegData: mongoose.model('Reg_data', Reg_data),
-  Test: mongoose.model('Test', Test),
-  Coach: mongoose.model('Coach', Coach),
+   Padavan: mongoose.model('Padavan', Padavan),
+   RegData: mongoose.model('Reg_data', Reg_data),
+   Test: mongoose.model('Test', Test),
+   Coach: mongoose.model('Coach', Coach),
 }
